9f48abf5f0fab7ed02cd35ac41d93b0494c148db
arti <arti@privategatevpn.com>
2025-10-26 23:08:31 +0300

    Style transform p.3

--- src/components/GroupBar/GroupBar.tsx
+++ src/components/GroupBar/GroupBar.tsx
@@ -2,14 +2,41 @@ import React from "react";
 import { Flex } from "../Flex";
 import { UserIcon } from "@/assets/icons/UserIcon";
 import styles from './styles.module.scss';
+import { APICall, getGroups } from "@/lib/calls";
+import { APIQuery } from "@/lib/forms";
+import { getLocal } from "@/lib/localstorage";
+import Link from "next/link";
 
 export const GroupBar : React.FC = () => {
+    const [groups,setGroups] = React.useState({});
+
+    const getGs = new APIQuery(
+        ["pk"],
+        getGroups as APICall
+    )
+    .addResponseTo(200, (r) => {
+        setGroups(JSON.parse(r));
+    }).callable()
+
+    const user = JSON.parse(getLocal("User"));
+    React.useEffect(()=> getGs({pk: user?.pk}),[]);
+
     return (
-    <div className={styles.bar}>
-        <h1 className={styles.title}>Чаты</h1>
-        <Flex gap="10px">
-            <UserIcon color="#aa2daaff"/>
+        <Flex 
+            direction="column"
+            gap="20px"
+            className={styles.group}
+        >
+            <h1 className={styles.title}>Чаты</h1>
+            {Object.values(groups).map((a : any) => (
+            <Flex justifyContent="flex-start">
+                <UserIcon color="#aa2daaff"/>
+                <Link
+                    className={styles.glink} 
+                    href={`/group?gpk=${a?.pk}`}
+                >{a?.name}</Link>
+            </Flex>
+            ))}
         </Flex>
-    </div>
     );
 }
